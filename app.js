c5d64a5f7a86bcf047e4cb7c8a4bc9f23f332edc
LillianWight <lillian.wight@gmail.com>
2022-04-22 16:35:30 -0600

    End of Lesson 16 Asynchronous programming

--- app.js
+++ app.js
@@ -12,4 +12,11 @@ console.log("TOTAL: ", total); */
 
 const fs = require("fs");
 const fileName = "target.txt";
-fs.watch(fileName, () => console.log(`File changed!`));
+fs.readFile(fileName, (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data.toString());
+});
+
+console.log("Node.js async programming :exploding_head:");
