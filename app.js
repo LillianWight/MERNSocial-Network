5f7c70acbc9f6deeecd7800c774594b520355fd8
LillianWight <lillian.wight@gmail.com>
2022-04-26 10:37:17 -0600

    End of Lesson 18 Functional Approach

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-/* const http = require("http");
-const { sum } = require("./helpers");
-
-const server = http.createServer((req, res) => {
-  res.end("Hello World from Node.js updated for real");
-});
-
-server.listen(5309);
-
-const total = sum(10, 200);
-console.log("TOTAL: ", total); */
-
 const fs = require("fs");
 const fileName = "target.txt";
+
+const errHandler = (err) => console.log(err);
+const dataHandler = (data) => console.log(data.toString());
+
 fs.readFile(fileName, (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(data.toString());
+  if (err) errHandler(err);
+  dataHandler(data);
 });
 
 console.log("Node.js async programming :exploding_head:");
